c81db38110538b634109d236cb61cabf6eb491d8
kob <kob8283@gmail.com>
2025-02-24 10:13:27 +0800

    Add files via upload

---
+++ save_text.php
@@ -0,0 +1,17 @@
+﻿<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $text = isset($_POST['text']) ? $_POST['text'] : '';
+    $filePath = 'output.txt';
+
+    // 将文本内容写入文件
+    if (file_put_contents($filePath, $text)) {
+        echo "Text has been saved to output.txt";
+    } else {
+        echo "Failed to save text to output.txt";
+    }
+} else {
+    echo "Invalid request method.";
+}
+
+?>
